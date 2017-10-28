f5c162623964db411e96f9d8847f47655fd11b3c
GracpAA <gracp97@mail.ru>
2017-10-28 22:48:54 +0200

    exercise 2

--- app/src/main/java/com/example/gracp/moneytracker/MainActivity.java
+++ app/src/main/java/com/example/gracp/moneytracker/MainActivity.java
@@ -2,16 +2,79 @@ package com.example.gracp.moneytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MyTag";
 
+    boolean isEnabled = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText item = findViewById(R.id.item);
+        final EditText cost = findViewById(R.id.cost);
+        final ImageButton add = findViewById(R.id.add);
+
+        add.setEnabled(false);
+
+        item.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(!TextUtils.isEmpty(charSequence)) isEnabled = true;
+
+                else add.setEnabled(false);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        cost.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if(!TextUtils.isEmpty(charSequence)&& isEnabled == true) add.setEnabled(true);
+
+                else add.setEnabled(false);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, R.string.add_text, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     @Override
